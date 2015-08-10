9a22bc9c76970b58c02ec2cd071ac82a377e2173
Lorenzo Stoakes <lstoakes@gmail.com>
2015-08-10 19:57:01 +0100

    Add a number of new diagnostics commands.

--- diagnose_template.sh
+++ diagnose_template.sh
@@ -24,7 +24,22 @@ fi
 # DIAGNOSTIC COMMANDS BELOW.
 # Prepend with 'd' due to issues with set -x.
 
+d uname -a
 d free -h
+d cat /proc/cpuinfo
 d cat /proc/meminfo
 d ps
 d top -b -n 1
+d rce --version
+d rce images
+d rce ps
+d tail -500 /var/log/supervisor-log/resin_supervisor_stdout.log
+d df -h
+d btrfs fi df /mnt/data-disk
+d cat /mnt/data-disk/config.json
+d mount
+d ls -l /dev
+d date
+d /sbin/ip addr
+d curl https://google.co.uk
+d ping -c 1 -W 3 google.co.uk
