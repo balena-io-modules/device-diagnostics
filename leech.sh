48cb7d101cbcc12f63f4d078302c913c4b2dc45d
Lorenzo Stoakes <lstoakes@gmail.com>
2015-12-09 16:57:27 +0000

    Remove requirement to know device VPN IP adddress.
    Use the new $uuid.vpn functionality on the VPN server to be able to 'leech' a
    device without having to lookup its VPN IP address.

--- leech.sh
+++ leech.sh
@@ -6,16 +6,15 @@ function fatal()
 	exit 1
 }
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    fatal usage: $(basename $0) [vpn ip address] [device uuid]
+if [ -z "$1" ]; then
+    fatal usage: $(basename $0) [device uuid]
 fi
 
 if [ ! -f diagnose_template.sh ]; then
     fatal Missing diagnose_template.sh file.
 fi
 
-ip=$1
-uuid=$2
+uuid=$1
 
 # Gets current script dir, see http://stackoverflow.com/a/246128.
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -29,7 +28,7 @@ mkdir /tmp/leech
 
 sed "s/@@replaceme@@/$uuid/" ${script_dir}/diagnose_template.sh > /tmp/leech/diagnose.sh
 
-ssh_opts="-o Hostname=$ip -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+ssh_opts="-o Hostname=$uuid.vpn -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 echo Copying script to device...
 scp -q -p $ssh_opts /tmp/leech/diagnose.sh resin:/home/root/ 2>/dev/null
