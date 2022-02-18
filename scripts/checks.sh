d5d4487af72a23f2062689a01127d2ea21d5f2b6
Balena CI <34882892+balena-ci@users.noreply.github.com>
2022-02-18 17:20:13 +0200

    v4.22.9

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-DIAGNOSE_VERSION=4.22.8
+DIAGNOSE_VERSION=4.22.9
 # Don't run anything before this source as it sets PATH here
 # shellcheck disable=SC1091
 source /etc/profile
